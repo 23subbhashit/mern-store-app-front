fc70d1948523fde162cc651ab2cdf129fa4c1df2
23subbhashit <43717493+23subbhashit@users.noreply.github.com>
2020-06-08 18:55:35 +0530

    second  commit

--- src/core/Home.js
+++ src/core/Home.js
@@ -3,12 +3,11 @@ import "../styles.css";
 import { API } from "../backend";
 import Base from "./Base";
 
-
 export default function Home() {
   console.log("API IS", API);
 
   return (
-    <Base title="Home Page">
+    <Base title="Home Page" description="Welcome to the Tshirt Store">
       <div className="row">
         <div className="col-4">
           <button className="btn btn-success">TEST</button>
